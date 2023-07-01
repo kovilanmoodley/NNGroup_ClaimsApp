1ce551f6d208667c9a0b594dd3a66af839e25c68
kovilanmoodley <kovilanmoodley0@gmail.com>
2023-07-01 11:05:06 +0200

    test

---
+++ NNGroup_DataManager/Controllers/ViewClaimStatusController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NNGroup_DataManager.DataAccess;
+using ShareModels.Models;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NNGroup_DataManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViewClaimStatusController : ControllerBase
+    {
+        public readonly IClaimDataAccess _context;
+        // GET: api/<ViewClaimController>
+        public ViewClaimStatusController(IClaimDataAccess context)
+        {
+            _context = context;
+        }
+
+
+        // GET api/<ViewClaimStatusController>/5
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<string> Get(int claimid, int clientid)
+        {
+            ClaimStatusChangeRequest claimStatusChangeRequest = new() { ClaimID = claimid, ID = clientid };
+
+            Claim claim = _context.ViewClaim(claimStatusChangeRequest)!;
+
+            if (claim == null)
+                return NotFound();
+            else
+                return Ok(claim.ClaimStatus);
+        }
+    }
+}
---
+++ NNGroup_DataManager/Helper/DataEncryption.cs
@@ -0,0 +1,25 @@
+﻿using ShareModels.Models;
+
+namespace NNGroup_DataManager.Helper
+{
+    public class DataEncryption
+    {
+        public static int Encrypt(int ID) 
+        { 
+            return (ID * -1); 
+        }
+        public static int Decrypt(int ID)
+        {
+            return (ID * -1);
+        }
+        public static void DecryptAudit(List<AuditClaim> auditList)
+        {
+            foreach(AuditClaim ac in auditList)
+            {
+                ac.Client.ClientID = Decrypt(ac.Client.ClientID);
+                ac.Employee.EmployeeID = Decrypt(ac.Employee.EmployeeID);
+            }
+        }
+
+    }
+}
