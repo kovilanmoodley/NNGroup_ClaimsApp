0ef5770e73c9b7dec806e3a56ebaa38194cfd06a
kovilanmoodley <kovilanmoodley0@gmail.com>
2023-07-01 11:04:53 +0200

    updated

---
+++ ShareModels/Models/ClaimRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareModels.Models
+{
+    public class ClaimRequest
+    {
+        public int ClientID { get; set; }
+        public string? ClaimDescription { get; set; }
+        public double ClaimAmount { get; set; } = 0;
+
+    }
+}
